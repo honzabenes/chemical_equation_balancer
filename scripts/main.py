93d3a8119db5d3cd2a58efcf8cb189649c954c1d
Jan <jan.b3nes@gmail.com>
2025-02-05 12:28:17 +0100

    header added

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,10 @@
+'''
+    Chemical equation balancer
+    Autor: Jan Beneš
+    1. ročník MFF UK obor Informatika, kruh 33
+    Zimní semestr 2024/2025
+    Programování I (NPRG030)
+'''
 
 # path in cmd: cd programovani/zapoctovy_program/chemistry/git_repo/zapoctovy_program
 # run command: cat input/file.txt | python scripts/main.py
